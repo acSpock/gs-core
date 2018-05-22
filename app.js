e5fd6c5663e08d754c89538b249ab20c70a41064
Andy Spock <Andy.Spock@mlb.com>
2018-05-22 01:38:16 -0700

    first go

---
+++ app.js
@@ -0,0 +1,14 @@
+let express = require('express');
+let app = express();
+let server = require('http').Server(app);
+
+app.use(express.static(__dirname + '/public'));
+
+
+// app.get('/*', function(req, res, next) {
+// 	res.sendFile(__dirname + '/index.html');
+// });
+
+server.listen(7070, ()=>{
+	console.log("Running 7070")
+});
